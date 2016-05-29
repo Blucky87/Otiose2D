8187cab4dd2bfe68b14885a823d75e1bbd62f43c
Brian Luckenbill <blucky87@gmail.com>
2016-05-29 16:05:38 -0400

    Squashed 'Dependencies/Nez/' content from commit 2271077
    git-subtree-dir: Dependencies/Nez
    git-subtree-split: 2271077b8dbc118dba2b19bc35c3221d9feae93e

---
+++ DefaultContentSource/effects/SpriteEffect.fx
@@ -0,0 +1,30 @@
+﻿
+Texture2D SpriteTexture;
+
+
+sampler2D SpriteTextureSampler = sampler_state
+{
+	Texture = <SpriteTexture>;
+};
+
+struct VertexShaderOutput
+{
+	float4 Position : POSITION;
+	float4 Color : COLOR0;
+	float2 TextureCoordinates : TEXCOORD0;
+};
+
+
+float4 MainPS( VertexShaderOutput input ) : COLOR
+{
+	return tex2D( SpriteTextureSampler, input.TextureCoordinates );
+}
+
+
+technique SpriteDrawing
+{
+	pass P0
+	{
+		PixelShader = compile ps_3_0 MainPS();
+	}
+};
---
+++ Nez-PCL/Utils/ReflectionUtils.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Reflection;
+
+
+namespace Nez
+{
+	/// <summary>
+	/// helper class to fetch property delegates
+	/// </summary>
+	class ReflectionUtils
+	{
+		public static FieldInfo getFieldInfo<T>( System.Object targetObject, string fieldName )
+		{
+			FieldInfo fieldInfo = null;
+			var type = targetObject.GetType();
+			do
+			{
+				fieldInfo = type.GetField( fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic );
+				type = type.BaseType;
+			} while ( fieldInfo == null && type != null );
+
+			return fieldInfo;
+		}
+
+
+		public static PropertyInfo getPropertyInfo( System.Object targetObject, string propertyName )
+		{
+			#if NETFX_CORE
+			return targetObject.GetType().GetRuntimeProperty( propertyName );
+			#else
+			return targetObject.GetType().GetProperty( propertyName, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public );
+			#endif
+		}
+
+
+		public static MethodInfo getMethodInfo( System.Object targetObject, string methodName )
+		{
+			#if NETFX_CORE
+			return targetObject.GetType().GetRuntimeMethod( propertyName );
+			#else
+			return targetObject.GetType().GetMethod( methodName, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public );
+			#endif
+		}
+
+
+		public static T createDelegate<T>( System.Object targetObject, MethodInfo methodInfo )
+		{
+			#if NETFX_CORE
+			// Windows Phone/Store new API
+			throw NotImplementedException();
+			#else
+			return (T)(object)Delegate.CreateDelegate( typeof( T ), targetObject, methodInfo );
+			#endif
+		}
+
+		
+		/// <summary>
+		/// either returns a super fast Delegate to set the given property or null if it couldn't be found
+		/// via reflection
+		/// </summary>
+		public static T setterForProperty<T>( System.Object targetObject, string propertyName )
+		{
+			// first get the property
+			var propInfo = getPropertyInfo( targetObject, propertyName );
+			if( propInfo == null )
+				return default(T);
+
+			return createDelegate<T>( targetObject, propInfo.GetSetMethod( true ) );
+		}
+
+
+		/// <summary>
+		/// either returns a super fast Delegate to get the given property or null if it couldn't be found
+		/// via reflection
+		/// </summary>
+		public static T getterForProperty<T>( System.Object targetObject, string propertyName )
+		{
+			// first get the property
+			var propInfo = getPropertyInfo( targetObject, propertyName );
+			if( propInfo == null )
+				return default(T);
+
+			return createDelegate<T>( targetObject, propInfo.GetGetMethod( true ) );
+		}
+
+	}
+}
+

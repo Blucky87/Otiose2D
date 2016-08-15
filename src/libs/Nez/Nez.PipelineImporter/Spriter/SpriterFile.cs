4d8165660ff7042967ff39e6eb30cd2a6c344b41
Brian Luckenbill <blucky87@gmail.com>
2016-08-15 02:28:20 -0400

    working

---
+++ src/libs/Nez/Nez.PipelineImporter/Spriter/SpriterFile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+
+
+namespace Nez.PipelineImporter.Spriter
+{
+  public class SpriterFile
+  {
+    [JsonProperty("frames")]
+    public List<TexturePackerRegion> regions;
+
+    [JsonProperty("meta")]
+    public TexturePackerMeta metadata;
+  }
+}
